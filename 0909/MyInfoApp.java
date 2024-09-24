cd6b29e7ab9a8e2aaaa0e491eb31c3a3ebce3510
sam3319 <58761373+sam3319@users.noreply.github.com>
2024-09-24 10:16:20 +0900

    Add files via upload

---
+++ 0909/MyInfoApp.java
@@ -0,0 +1,7 @@
+public class MyInfoApp {
+	public static void main(String[] args) {
+		System.out.println("DongHyun Lee");
+		System.out.println("27 years old");
+		System.out.println("Department of Computer Software");
+	}
+}
